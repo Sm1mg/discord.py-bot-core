73912e9e79b86e7a29d36724a03a9e220cc076e3
Sm1mg <noahnmj@gmail.com>
2023-11-22 16:19:40 -0500

    cleanup of functions

--- musS_D.py
+++ musS_D.py
@@ -11,55 +11,53 @@ import Utils
 load_dotenv()  # getting the key from the .env file
 key = os.environ.get('key')
 
-
 class Bot(commands.Bot):  # initiates the bots intents and on_ready event
     def __init__(self):
         intents = discord.Intents.default()
-
         #-------------------
         # Set command prefix
         #-------------------
         super().__init__(command_prefix="​", intents=intents)
-        self.synced=False
 
-    async def on_ready(self):
+    async def setup_hook(self):
         #---------
         # Add cogs
         #---------
-        await bot.load_extension("cogs.BaseCog")
+        await self.load_extension("cogs.BaseCog")
         Utils.pront("Cogs loaded!")
 
-        #syncing tree
+    async def on_ready(self):
+        # Command syncing
         Utils.pront("Syncing tree")
-        if not self.synced:
-            await bot.tree.sync()  # please dont remove just in case i need to sync
+        await self.tree.sync()
         Utils.pront("Tree synced!")
-
-        #setting status
-        Utils.pront("Setting bot status")
-        await self.change_presence(activity=discord.Activity(
-            type=discord.ActivityType.watching, name=f"you in {len(bot.guilds):,} Servers."))
         
         Utils.pront("Bot is ready", lvl="OKGREEN")
 
-# Global Variables
+# Initialize bot object
 bot = Bot()
-
-# Ping command
-@bot.tree.command(name="ping", description="Ping command")
-async def _help(interaction: discord.Interaction) -> None:
-    await Utils.send(interaction, 'Pong!')
 
 # Custom error handler
 async def on_tree_error(interaction: discord.Interaction, error: discord.app_commands.AppCommandError):
 
 
     # Fallback default error
-    await interaction.channel.send(embed=Utils.get_embed(interaction, title="An error occurred.", content=f'```ansi\n{error}```', progress=False))
+    await interaction.channel.send(
+        embed=Utils.get_embed(interaction,
+            title="An error occurred.",
+            content=f'```ansi\n{error}```'
+            )
+        )
     # Allows entire error to be printed without raising an exception
     # (would create an infinite loop as it would be caught by this function)
     traceback.print_exc()
+# Set error handler method
 bot.tree.on_error = on_tree_error
 
+
+# Ping command
+@bot.tree.command(name="ping", description="Ping command")
+async def _help(interaction: discord.Interaction) -> None:
+    await Utils.send(interaction, 'Pong!')
 
 bot.run(key)
